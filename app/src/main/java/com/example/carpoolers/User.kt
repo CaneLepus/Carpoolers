be9c70f3cf65795096ff62e103a80c7876a6c1a1
Felix <felixkivioja95@gmail.com>
2021-03-01 12:51:35 +0100

    Added rating to user class

--- app/src/main/java/com/example/carpoolers/User.kt
+++ app/src/main/java/com/example/carpoolers/User.kt
@@ -1,13 +1,14 @@
 package com.example.carpoolers
 
-data class User(val first: String, val second: String, val phone: String, val email: String, val password: String) {
-    public fun storeFormat(): HashMap<String, String>{
+data class User(val first: String, val second: String, val phone: String, val email: String, val password: String, val rating: Double) {
+    fun storeFormat(): HashMap<Any, Any>{
         return hashMapOf(
             "first" to first,
             "last" to second,
             "phone" to phone,
             "email" to email,
-            "password" to password
+            "password" to password,
+            "rating" to rating
         )
     }
 }
