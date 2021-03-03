c7a29238f3e025e2964152e24dc8adb1fcc93b3f
simonS95 <simon.sorensen@outlook.com>
2021-03-03 16:21:57 +0100

    Mindre tillägg

--- app/src/main/java/com/example/carpoolers/NotificationHandler.kt
+++ app/src/main/java/com/example/carpoolers/NotificationHandler.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Tasks
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
@@ -26,9 +27,6 @@ class NotificationHandler: FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         Log.d("NEW TOKEN", "Refreshed token: $token")
 
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // FCM registration token to your app server.
         sendRegistrationToServer(token)
 
     }
@@ -48,28 +46,23 @@ class NotificationHandler: FirebaseMessagingService() {
             }
 
             // Get new FCM registration token
+            Tasks.await(task)
             val token = task.result
 
             Log.d("CURRENT TOKEN", "$token")
         })
 
         Thread.sleep(1000)
-        return "${snapshot.result}"
+
+        var returnToken = "not a success"
+        if (snapshot.isSuccessful){
+            returnToken = "${snapshot.result}"
+        }
+
+        return returnToken
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        // [START_EXCLUDE]
-        // There are two types of messages data messages and notification messages. Data messages are handled
-        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
-        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
-        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
-        // When the user taps on the notification they are returned to the app. Messages containing both notification
-        // and data payloads are treated as notification messages. The Firebase console always sends notification
-        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
-        // [END_EXCLUDE]
-
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d("REMOTE MESSAGE", "From: ${remoteMessage.from}")
 
         // Check if message contains a data payload.
