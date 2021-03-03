4f2f7815352ee782a6ac71782bca6e80ece28559
Felix <felixkivioja95@gmail.com>
2021-03-03 13:26:47 +0100

    Added array list of ratings

--- app/src/main/java/com/example/carpoolers/User.kt
+++ app/src/main/java/com/example/carpoolers/User.kt
@@ -1,15 +1,19 @@
 package com.example.carpoolers
 
-data class User(val first: String, val second: String, val phone: String, val lat: Double, val long: Double, val bio: String) {
-    fun storeFormat(): HashMap<Any, Any> {
+data class User(val first: String, val second: String, val phone: String, val lat: Double, val long: Double, val bio: String, val ratings:ArrayList<Double>) {
+    fun storeFormat(): HashMap<Any, Any>{
+
+        val rating: ArrayList<Double> = ArrayList()
+        rating.add(0.0)
+
         return hashMapOf(
-                "first" to first,
-                "last" to second,
-                "phone" to phone,
-                "rating" to 0,
-                "latitude" to lat,
-                "longitude" to long,
-                "biography" to bio
+            "first" to first,
+            "last" to second,
+            "phone" to phone,
+            "rating" to 0,
+            "latitude" to lat,
+            "longitude" to long,
+            "biography" to bio
         )
     }
 }
