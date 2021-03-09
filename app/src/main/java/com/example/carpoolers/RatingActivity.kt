eb16792db8940a50ea2599631ceb03a19f867f13
Felix <felixkivioja95@gmail.com>
2021-03-09 14:06:26 +0100

    Added functionality so that u can see your own rating

--- app/src/main/java/com/example/carpoolers/RatingActivity.kt
+++ app/src/main/java/com/example/carpoolers/RatingActivity.kt
@@ -2,36 +2,83 @@ package com.example.carpoolers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.widget.Button
 import android.widget.RatingBar
 import android.widget.Toast
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.getField
 import com.google.firebase.ktx.Firebase
+import java.util.*
+import kotlin.collections.ArrayList
 
 class RatingActivity : AppCompatActivity() {
 
     val db = Firebase.firestore
+    val auth = Firebase.auth
     lateinit var rating: RatingBar
     lateinit var button: Button
+    private val users = db.collection("users")
+    private val query = users.document(auth.currentUser.uid)
+    var arrList : ArrayList<Double> = ArrayList()
+    var finalRating = 0.0
+
+
+    val ratings : ArrayList<Double> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_rating)
 
         rating = findViewById(R.id.ratingBar)
         button = findViewById(R.id.button)
 
+        getRatingsFromDb()
 
-        rating.rating= 1.0F //this is gonna need to get db value later
+
+        Log.d("-----------------", arrList.toString())
+
+        //rating.rating= finalRating.toFloat() //this is gonna need to get db value later
 
         button.setOnClickListener{test()}
     }
 
 
-    private fun test(){
+    fun getRatingsFromDb() {
 
-        Toast.makeText(this, "You gave a rating of " + rating.rating + " stars", Toast.LENGTH_LONG)
 
+        query.get().addOnSuccessListener { document ->
+            arrList = document.get("rating") as ArrayList<Double>
+
+            for(item in arrList){
+                finalRating += item
+            }
+
+            finalRating /= arrList.size
+
+            rating.rating = finalRating.toFloat()
+
+            Toast.makeText(
+                    this, arrList.toString(),
+                    Toast.LENGTH_SHORT
+            ).show()
+
+        }.addOnFailureListener {
+            Toast.makeText(
+                    this, "Rating could not be initialized",
+                    Toast.LENGTH_SHORT
+            ).show()
+        }
 
     }
-}
+
+    private fun test(){
+
+        Toast.makeText(this, "You gave a rating of " + rating.rating + " stars", Toast.LENGTH_LONG).show()
+        Log.i(".RatingActivity","Rating given: " + rating.rating)
+
+    }
+    }
