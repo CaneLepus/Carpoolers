800346127a8996fbe1a20b80d90e7373ae322979
Felix <felixkivioja95@gmail.com>
2021-03-09 16:31:32 +0100

    Temp rating available on temp profile page

--- app/src/main/java/com/example/carpoolers/SwipeFunction/TemporaryProfileActivity.java
+++ app/src/main/java/com/example/carpoolers/SwipeFunction/TemporaryProfileActivity.java
@@ -3,11 +3,17 @@ package com.example.carpoolers.SwipeFunction;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.Telephony;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.example.carpoolers.R;
+import com.example.carpoolers.Rate;
+
 
 public class TemporaryProfileActivity extends AppCompatActivity {
+
+    Rate ra = new Rate();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,29 @@ public class TemporaryProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_temporary_profile);
 
         TextView textView = findViewById(R.id.textView);
+        RatingBar ratingBar = findViewById(R.id.ratingBar);
         textView.setText(getIntent().getStringExtra("param"));
+        ratingBar.setRating(getIntent().getFloatExtra("rating", 0.0f));
+
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            public void onRatingChanged(RatingBar ratingBar, float rating,
+                                        boolean fromUser) {
+                rate(rating);
+                ratingBar.setIsIndicator(true);
+
+                ratingBar.setRating(ra.getRating(getApplicationContext(), getIntent().getStringExtra("uid")));
+
+                //ratings = String.valueOf(rating);
+
+            }
+        });
     }
+
+
+    void rate(Float rating){
+        ra.setRating(getApplicationContext(), getIntent().getStringExtra("uid"), rating);
+    }
+
+
 }
